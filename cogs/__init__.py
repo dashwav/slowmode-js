21087aa65bbe5e2d94860d8fdce4897713774275
dashwav <dashwav@gmail.com>
2018-09-11 11:48:51 -0700

    Remove unused files

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,11 +1,7 @@
 from cogs.spoils import Spoils
-from cogs.fightclub import Fightclub
 from cogs.owner import Owner
 from cogs.reactions import Reactions
-from cogs.logging import Logging
 from cogs.filter import Filter
-from cogs.janitor import Janitor
 from cogs.moderation import Moderation
 from cogs.stats import Stats
-from cogs.channels import Channels
-__all__ = ['Janitor', 'Fightclub', 'Channels', 'Reactions', 'Filter', 'Logging', 'Spoils', 'Moderation','Owner', 'Stats']
+__all__ = ['Reactions', 'Filter', 'Spoils', 'Moderation','Owner', 'Stats']
--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -1,4 +1,3 @@
 from .db_utils import PostgresController
-from .enums import Action, Change
 
-__all__ = ['PostgresController', 'Action', 'Change']
+__all__ = ['PostgresController',]
